c1e0206a8099015e74b4c07b399ff680cad25aa9
availableeve <87888113+availableeve@users.noreply.github.com>
2023-09-07 22:51:02 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,3 @@
-
+loadstring(game:HttpGet('https://raw.githubusercontent.com/availableeve/mortem-metallum/main/destroy.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/availableeve/mortem-metallum/main/sprint.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/availableeve/mortem-metallum/main/dupe.lua'))()
