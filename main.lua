fdd1d6ef85c3ff24636927cd4e1ea74e7c0a06c0
availableeve <87888113+availableeve@users.noreply.github.com>
2023-09-08 13:48:38 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,14 +1,3 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/availableeve/mortem-metallum/main/dupe.lua'))()
-wait(0.2)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/availableeve/mortem-metallum/main/destroy.lua'))()
-wait(0.2)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/availableeve/mortem-metallum/main/sprint.lua'))()
-
-
-
-
-
-
 local UserInputService = game:GetService("UserInputService")
 local player = game.Players.LocalPlayer
 
@@ -29,3 +18,37 @@ end
 
 UserInputService.InputBegan:Connect(onKey2Press)
 UserInputService.InputEnded:Connect(onKey2Release)
+
+local function onKeyPress(input)
+    if input.KeyCode == Enum.KeyCode.P then
+        for _ = 1, 50 do
+            local args = {
+                [1] = 0,
+                [2] = "0:0:0:0"
+            }
+
+            local ReplicatedStorage = game:GetService("ReplicatedStorage")
+            local ItemRemote = ReplicatedStorage:FindFirstChild("Item")
+            if ItemRemote then
+                ItemRemote:FireServer(unpack(args))
+            end
+        end
+    end
+end
+
+UserInputService.InputBegan:Connect(onKeyPress)
+
+local function onKey1Press(input)
+    if input.KeyCode == Enum.KeyCode.O then
+        local backpack = player:FindFirstChild("Backpack")
+        if backpack then
+            for _, item in pairs(backpack:GetChildren()) do
+                if item:IsA("Tool") and not table.find(theseones, item.Name) then
+                    item:Destroy()
+                end
+            end
+        end
+    end
+end
+
+UserInputService.InputBegan:Connect(onKey1Press)
