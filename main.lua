0245e8aef67c7dc23363f7905d7b73c78ee55988
availableeve <87888113+availableeve@users.noreply.github.com>
2023-09-08 15:06:50 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,65 +1,48 @@
 local players = game:GetService("Players"):GetPlayers()
 local UserInputService = game:GetService("UserInputService")
 local player = game.Players.LocalPlayer
-local w = {
+local wae = {
     "Fire bomb"
 }
---functions
---dupe
-local function dupe(input)
-    
-    if input.KeyCode == Enum.KeyCode.P then
-		
-        for _ = 1, 30 do
-            local ohNumber1 = 16
-		    local ohString2 = "0:0:0:0"
 
-		    game:GetService("ReplicatedStorage").Item:FireServer(ohNumber1, ohString2)
-		
+local function dupe(input)
+    if input.KeyCode == Enum.KeyCode.P then
+        for _ = 1, 30 do
+            local ohNumber1 = 0
+            local ohString2 = "0:0:0:0"
+            game:GetService("ReplicatedStorage").Item:FireServer(ohNumber1, ohString2)
+        end
     end
 end
-
--- destroy items
+UserInputService.InputBegan:Connect(dupe)
 local function destroy(input)
     if input.KeyCode == Enum.KeyCode.O then
         local backpack = player:FindFirstChild("Backpack")
         if backpack then
             for _, item in pairs(backpack:GetChildren()) do
-                if item:IsA("Tool") and not table.find(item.Name, "w") then
+                if item:IsA("Tool") and table.find(wae, item.Name)
                     item:Destroy()
                 end
             end
         end
     end
 end
-
--- walkspeed
+UserInputService.InputBegan:Connect(destroy)
 local function walkspeed24(input)
     if input.KeyCode == Enum.KeyCode.LeftControl then
         player.Character:FindFirstChild("Humanoid").WalkSpeed = 24.11
     end
 end
-
+UserInputService.InputBegan:Connect(walkspeed24)
 local function walkspeed16(input)
     if input.KeyCode == Enum.KeyCode.LeftControl then
         player.Character:FindFirstChild("Humanoid").WalkSpeed = 16
     end
 end
--- walkspeed
-UserInputService.InputBegan:Connect(walkspeed24)
 UserInputService.InputEnded:Connect(walkspeed16)
 
--- dupe
-UserInputService.InputBegan:Connect(dupe)
-
--- destroy
-UserInputService.InputBegan:Connect(destroy)
-
--- jumpcooldown
 for _, player in pairs(players) do
-    -- Check if the player has a character and a "JumpCooldown" property
     if player.Character and player.Character:FindFirstChild("JumpCooldown") then
-        -- Disable the "JumpCooldown" property for the player's character
         player.Character.JumpCooldown.Disabled = true
     end
 end
