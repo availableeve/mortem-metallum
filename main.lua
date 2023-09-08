668b04444d64aa0f289381cc7344b9de453abd8a
availableeve <87888113+availableeve@users.noreply.github.com>
2023-09-08 14:41:11 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,57 +1,55 @@
--- variables
+local players = game:GetService("Players"):GetPlayers()
 local UserInputService = game:GetService("UserInputService")
 local player = game.Players.LocalPlayer
-local walkspeed = 16
-local increasedWalkspeed = 24.11
-local w = {
-    "Fire bomb",
-    "",
-    "",
-    "",
-    "",
-    
-}
--- functions
-local function onKey2Press(input)
-    if input.KeyCode == Enum.KeyCode.LeftControl then
-        player.Character:FindFirstChild("Humanoid").WalkSpeed = increasedWalkspeed
-    end
-end
-UserInputService.InputBegan:Connect(onKey2Press)
-local function onKey2Release(input)
-    if input.KeyCode == Enum.KeyCode.LeftControl then
-        player.Character:FindFirstChild("Humanoid").WalkSpeed = walkspeed
-    end
-end
-UserInputService.InputEnded:Connect(onKey2Release)
-local function onKeyPress(input)
-    if input.KeyCode == Enum.KeyCode.P then
-        for _ = 1, 30 do
-            local args = {
-                [1] = 0,
-                [2] = "0:0:0:0"
-            }
+--functions
+--dupe
+local function dupe(input)
+    if input.KeyCode == Enum.KeyCode.K then
+		
+		local ohNumber1 = 16
+		local ohString2 = "0:0:0:0"
 
-            local ReplicatedStorage = game:GetService("ReplicatedStorage")
-            local ItemRemote = ReplicatedStorage:FindFirstChild("Item")
-            if ItemRemote then
-                ItemRemote:FireServer(unpack(args))
-            end
-        end
+		game:GetService("ReplicatedStorage").Item:FireServer(ohNumber1, ohString2)
+		
+		local ohNumber1 = 16
+		local ohString2 = "0:0:0:0"
+
+		game:GetService("ReplicatedStorage").Item:FireServer(ohNumber1, ohString2)
+		
+		local ohNumber1 = 16
+		local ohString2 = "0:0:0:0"
+
+		game:GetService("ReplicatedStorage").Item:FireServer(ohNumber1, ohString2)
+		
+		
+		
     end
 end
-UserInputService.InputBegan:Connect(onKeyPress)
-local function onKey1Press(input)
-    if input.KeyCode == Enum.KeyCode.O then
-        local backpack = player:FindFirstChild("Backpack")
-        if backpack then
-            for _, item in pairs(backpack:GetChildren()) do
-                if item:IsA("Tool") and not table.find(w, item.Name) then
-                    item:Destroy()
-                end
-            end
-        end
+-- walkspeed
+local function walkspeed24(input)
+    if input.KeyCode == Enum.KeyCode.LeftControl then
+        player.Character:FindFirstChild("Humanoid").WalkSpeed = 24.11
     end
 end
 
-UserInputService.InputBegan:Connect(onKey1Press)
+local function walkspeed16(input)
+    if input.KeyCode == Enum.KeyCode.LeftControl then
+        player.Character:FindFirstChild("Humanoid").WalkSpeed = 16
+    end
+end
+
+-- walkspeed
+UserInputService.InputBegan:Connect(walkspeed24)
+UserInputService.InputEnded:Connect(walkspeed16)
+
+-- dupe
+UserInputService.InputBegan:Connect(dupe)
+
+-- jumpcooldown
+for _, player in pairs(players) do
+    -- Check if the player has a character and a "JumpCooldown" property
+    if player.Character and player.Character:FindFirstChild("JumpCooldown") then
+        -- Disable the "JumpCooldown" property for the player's character
+        player.Character.JumpCooldown.Disabled = true
+    end
+end
